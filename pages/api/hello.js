b319d3c4f6ec4db6c1f19ad7fd28aee8e110b347
itishprasad30 <itishprasad20@gmail.com>
2022-03-17 20:18:14 +0530

    mirar changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,21 +5,21 @@ export default function handler(req, res) {
 
   // const ipAddress = req.headers["x-forwarded-for"] || "0.0.0.0";
 
-  try {
-    // if (req.method === "POST") {
-    //   const ipV4 = ipAddress;
-    //   res.status(201).json({
-    //     slug,
-    //     ipV4,
-    //   });
-    // } else {
-    //   res.status(405).json({ message: "Method Not Allowed" });
-    // }
-  } catch (er) {
-    console.log(err);
-    res
-      .status(505)
-      .json({ message: err.message ?? "Internal error in you phone" });
-  }
+  // try {
+  //   // if (req.method === "POST") {
+  //   //   const ipV4 = ipAddress;
+  //   //   res.status(201).json({
+  //   //     slug,
+  //   //     ipV4,
+  //   //   });
+  //   // } else {
+  //   //   res.status(405).json({ message: "Method Not Allowed" });
+  //   // }
+  // } catch (er) {
+  //   console.log(err);
+  //   res
+  //     .status(505)
+  //     .json({ message: err.message ?? "Internal error in you phone" });
+  // }
   res.status(200).json({ name: "John Doe" });
 }
