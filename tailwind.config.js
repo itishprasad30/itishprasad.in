4f3012bb84fa65feaa922b5d799d99d184c4c3cb
itishprasad30 <itishprasad20@gmail.com>
2022-03-23 22:17:39 +0530

    changes are made

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      // fontFamily: {
+      //   primary: ["Inter", ...fontFamily.sans],
+      // },
+      keyframes: {
+        flicker: {
+          "0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%": {
+            opacity: 0.99,
+            filter:
+              "drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))",
+          },
+          "20%, 21.999%, 63%, 63.999%, 65%, 69.999%": {
+            opacity: 0.4,
+            filter: "none",
+          },
+        },
+        shimmer: {
+          "0%": {
+            backgroundPosition: "-700px 0",
+          },
+          "100%": {
+            backgroundPosition: "700px 0",
+          },
+        },
+      },
+      animation: {
+        flicker: "flicker 3s linear infinite",
+        shimmer: "shimmer 1.3s linear infinite",
+      },
+    },
     textIndent: (theme, { negative }) => ({
       ...{
         sm: "2rem",
