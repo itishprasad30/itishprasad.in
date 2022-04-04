f5964df128fe306c3b1a68723cafdde5348547c1
itishprasad30 <itishprasad20@gmail.com>
2022-04-04 23:16:18 +0530

    tailwind purge - content waring fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
