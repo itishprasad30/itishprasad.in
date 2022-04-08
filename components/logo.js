951dc16dc53fbfffc39562bc89928da7439a55ef
itishprasad30 <itishprasad20@gmail.com>
2022-04-08 16:03:26 +0530

    logo changes

--- components/logo.js
+++ components/logo.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import UnderLineLink from "./links/UnderlineLink";
 
 const Logo = () => {
   const footPrintImg = `/images/Logo.png`;
@@ -7,7 +8,7 @@ const Logo = () => {
   return (
     <Link href="/">
       <a>
-        <div className="flex flex-row">
+        <div className="flex flex-row gap-2">
           <Image
             src={footPrintImg}
             width={20}
@@ -15,9 +16,15 @@ const Logo = () => {
             alt="logo"
             className="rounded"
           />
-          <p className="ml-3 rounded-lg font-serif font-bold text-gray-700 underline-offset-4 transition-colors hover:underline dark:text-white  ">
+          <UnderLineLink
+            href="/"
+            className="font-serif text-2xl font-bold leading-tight"
+          >
             Itish Prasad
-          </p>
+          </UnderLineLink>
+          {/* <p className="ml-3 rounded-lg font-serif font-bold text-gray-700 underline-offset-4 transition-colors hover:underline dark:text-white  ">
+            Itish Prasad
+          </p> */}
         </div>
       </a>
     </Link>
