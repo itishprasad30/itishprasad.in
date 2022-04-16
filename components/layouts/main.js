d88ce724dad5e7a90bbb93eb2d05354ad87fff61
itishprasad30 <itishprasad20@gmail.com>
2022-04-16 11:53:25 +0530

    some max-w changes for the window view

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,7 +10,7 @@ const Main = ({ children, router }) => {
     <div className="space-y-9 bg-[#F0E7DB] p-4 dark:bg-[#202023]">
       <Seo templateTitle="Itish Prasad " />
       <Navbar path={router.asPath} />
-      <div className="mx-auto max-w-5xl  px-4 pt-4 md:px-20">
+      <div className="mx-auto max-w-6xl  px-4 pt-4 md:px-20">
         {/* <NoSsr>
           <VoxelDog />
         </NoSsr> */}
