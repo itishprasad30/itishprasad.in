5f71d543691e19e78571a551f6a6c949dd3cb07f
itishprasad30 <itishprasad20@gmail.com>
2022-04-20 21:55:59 +0530

    trackevent these two button

--- components/metrics/Github.js
+++ components/metrics/Github.js
@@ -2,6 +2,9 @@ import useSWR from "swr";
 import format from "comma-number";
 
 import MetricCard from "./Card";
+import ButtonLink from "../links/ButtonLink";
+import clsx from "clsx";
+import { trackEvent } from "../../lib/analytics";
 
 export default function GitHub() {
   const fetcher = (url) => fetch(url).then((r) => r.json());
@@ -9,14 +12,39 @@ export default function GitHub() {
 
   const stars = format(data?.stars);
 
-  const link = "https://github.com/itishprasad30";
+  const link = "https://github.com/itishprasad30?tab=stars";
 
   return (
-    <MetricCard
-      header="GitHub Stars"
-      link={link}
-      metric={stars}
-      gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
-    />
+    <div className="group relative">
+      <div
+        className={clsx(
+          "animate-tilt absolute -inset-0.5 mt-6 rounded blur",
+          "bg-gradient-to-r from-yellow-300 to-green-500",
+          "dark:from-yellow-400 dark:via-pink-300",
+          "opacity-75 transition duration-1000 group-hover:opacity-100 group-hover:duration-200"
+        )}
+      />
+      <ButtonLink
+        className="relative mt-6 flex w-full flex-col bg-[#F0E7DB] dark:bg-[#202023] dark:text-white"
+        href={link}
+        variant="light"
+        onClick={() => trackEvent(`DashBoardLink: Github Stars `, "Button")}
+      >
+        Github Stars ⭐
+        <p
+          className=" text-xl font-bold
+        "
+        >
+          {stars || "-"}
+        </p>
+      </ButtonLink>
+    </div>
   );
 }
+
+// <MetricCard
+//   header="GitHub Stars"
+//   link={link}
+//   metric={stars}
+//   gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
+// />
--- components/metrics/Repo.js
+++ components/metrics/Repo.js
@@ -2,6 +2,9 @@ import useSWR from "swr";
 import format from "comma-number";
 
 import MetricCard from "./Card";
+import ButtonLink from "../links/ButtonLink";
+import clsx from "clsx";
+import { trackEvent } from "../../lib/analytics";
 
 export default function GitHub() {
   const fetcher = (url) => fetch(url).then((r) => r.json());
@@ -9,14 +12,41 @@ export default function GitHub() {
 
   const repoCount = format(data?.repoCount);
 
-  const link = "https://github.com/itishprasad30";
+  const link = "https://github.com/itishprasad30/?tab=repositories";
 
   return (
-    <MetricCard
+    <div className="group relative">
+      <div
+        className={clsx(
+          "animate-tilt absolute -inset-0.5 mt-6 rounded blur",
+          "bg-gradient-to-r from-yellow-300 to-green-500",
+          "dark:from-yellow-400 dark:via-pink-300",
+          "opacity-75 transition duration-1000 group-hover:opacity-100 group-hover:duration-200"
+        )}
+      />
+      <ButtonLink
+        className="relative mt-6 flex w-full flex-col bg-[#F0E7DB] dark:bg-[#202023] dark:text-white"
+        href={link}
+        variant="light"
+        onClick={() => trackEvent(`DashBoardLink: Github Repos `, "Button")}
+      >
+        Github Repositories 🧵{" "}
+        <p
+          className=" text-xl font-bold
+        "
+        >
+          {repoCount || "-"}
+        </p>
+      </ButtonLink>
+    </div>
+  );
+}
+
+{
+  /* <MetricCard
       header="GitHub Repos"
       link={link}
       metric={repoCount}
       gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
-    />
-  );
+    /> */
 }
