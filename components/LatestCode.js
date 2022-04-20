1a4a445100c1b86e7a66c8d7a4f9d13f561099cd
itishprasad30 <itishprasad20@gmail.com>
2022-04-20 21:08:00 +0530

    button added and dashboard hover effect

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import getLatestRepos from "../lib/getLatestRepos";
 import userData from "../Constants/data";
+import ButtonLink from "./links/ButtonLink";
+import clsx from "clsx";
 
 export default function LatestCode({ repositories }) {
   const [repos, setRepos] = useState([]);
@@ -14,32 +16,27 @@ export default function LatestCode({ repositories }) {
   return (
     <div>
       <div>
-        <div className="flex flex-col items-center justify-between md:flex-row ">
+        <div className="mb-6 flex flex-col items-center justify-between md:flex-row ">
           <h1 className="py-20  text-4xl font-bold md:text-left  md:text-4xl">
             Latest Code
           </h1>
 
-          <a
-            href={`https://github.com/${userData.githubUsername}`}
-            className="mb-20 flex flex-row items-center space-x-4 rounded-lg bg-gray-50 px-8 py-4 text-lg font-semibold shadow-lg hover:bg-gray-200 dark:text-gray-700 md:mb-0"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-arrow-up-right-square"
-              stroke="4"
-              strokeWidth="4"
-              viewBox="0 0 16 16"
+          <div className="group relative">
+            <div
+              className={clsx(
+                "animate-tilt absolute -inset-0.5  rounded blur",
+                "bg-gradient-to-r from-yellow-300 to-green-500",
+                "dark:from-yellow-400 dark:via-pink-300",
+                "opacity-75 transition duration-1000 group-hover:opacity-100 group-hover:duration-200"
+              )}
+            />
+            <ButtonLink
+              className="relative  bg-[#F0E7DB] dark:bg-[#202023] dark:text-white"
+              href={`https://github.com/${userData.githubUsername}/?tab=repositories`}
             >
-              <path
-                fillRule="evenodd"
-                d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"
-              />
-            </svg>
-            <p>View GitHub</p>
-          </a>
+              View In Github
+            </ButtonLink>
+          </div>
         </div>
       </div>
       <div className="mx-auto grid max-w-6xl grid-cols-1 gap-10 gap-y-20 px-10 md:grid-cols-2 lg:-mt-10 lg:grid-cols-3">
--- components/Track.js
+++ components/Track.js
@@ -1,19 +1,19 @@
 export default function Track(track) {
   return (
-    <div className="flex flex-row items-baseline border-b border-gray-200 dark:border-gray-800 max-w-3xl w-full mt-8">
+    <div className="mt-8 flex w-full max-w-3xl cursor-pointer flex-row items-baseline rounded-md border-b border-gray-200 p-2 hover:bg-gray-300 dark:border-gray-800 dark:hover:bg-gray-800">
       <p className="text-sm font-bold text-gray-400 dark:text-gray-600">
         {track.ranking}
       </p>
       <div className="flex flex-col pl-4">
         <a
-          className="font-medium text-gray-900 dark:text-gray-100 truncate w-60 sm:w-96 md:w-full"
+          className="w-60 truncate font-medium text-gray-900 dark:text-gray-100 sm:w-96 md:w-full"
           href={track.songUrl}
           target="_blank"
           rel="noopener noreferrer"
         >
           {track.title}
         </a>
-        <p className="text-gray-400 mb-4 truncate w-60 sm:w-96 md:w-full">
+        <p className="mb-4 w-60 truncate text-gray-400 sm:w-96 md:w-full">
           {track.artist}
         </p>
       </div>
