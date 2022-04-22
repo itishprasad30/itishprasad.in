02dfab926c82b43d0f1bed04d0a235cc24236103
itishprasad30 <itishprasad20@gmail.com>
2022-04-22 20:50:02 +0530

    greeting message added in website for production only

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -5,8 +5,17 @@ import NoSsr from "../no-ssr";
 import VoxelDog from "../voxel-dog";
 import Seo from "./Seo";
 import Header from "./Header";
+import { sayHelloFlag } from "../../Constants/env";
+import sayHello from "../../lib/sayHello";
+
+let saidHello = !sayHelloFlag;
 
 const Main = ({ children, router }) => {
+  if (!saidHello) {
+    sayHello();
+    saidHello = true;
+  }
+
   return (
     <div className="space-y-9 bg-[#F0E7DB] p-4 dark:bg-[#202023]">
       <Seo templateTitle="Itish Prasad " />
